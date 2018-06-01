19943b1597e38088739b95c066ffdc5fbd278f86
Nicholas <nenglish443@outlook.com>
2018-06-01 13:53:46 +0000

    Apply fixes from StyleCI

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,9 +1,11 @@
 <?php
+
 declare(strict_types=1);
 /**
  * A PSR-7 compliant http request handler.
  *
  * @license <https://github.com/Auvipev/Royal/master/LICENSE>.
+ *
  * @link    <https://github.com/Auvipev/Royal>.
  */
 
