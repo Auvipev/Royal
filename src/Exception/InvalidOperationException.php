95a5390676d016c59095eaeae1e898a9c11c4267
Nicholas <nenglish443@outlook.com>
2018-06-01 10:28:50 -0400

    Add the invalid operation exception.

---
+++ src/Exception/InvalidOperationException.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * A PSR-7 compliant http request handler.
+ *
+ * @license <https://github.com/Auvipev/Royal/master/LICENSE>.
+ *
+ * @link    <https://github.com/Auvipev/Royal>.
+ */
+
+namespace Auvipev\Royal;
+
+use RuntimeException;
+
+/**
+ * @class      InvalidOperationException.
+ * @extends    RuntimeException.
+ * @implements ExceptionInterface.
+ */
+class InvalidOperationException extends RuntimeException implements ExceptionInterface
+{
+}
