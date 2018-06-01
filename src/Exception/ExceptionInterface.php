2f6431fa87d1f771ee897d9b5380b03deb24ea67
Nicholas <nenglish443@outlook.com>
2018-05-31 21:46:52 -0400

    Add the exception interface.

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * A PSR-7 compliant http request handler.
+ *
+ * @license <https://github.com/Auvipev/Royal/master/LICENSE>.
+ * @link    <https://github.com/Auvipev/Royal>.
+ */
+
+namespace Auvipev\Royal;
+
+use Throwable;
+
+/**
+ * @interface ExceptionInterface.
+ * @extends   Throwable.
+ */
+class ExceptionInterface extends Throwable
+{
+}
