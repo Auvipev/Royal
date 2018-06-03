fba4e172b591df46610e7ad0427ffb8701624390
Nicholas <nenglish443@outlook.com>
2018-06-03 00:47:24 -0400

    Add response class.

---
+++ src/Response.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * A PSR-7 compliant http request handler.
+ *
+ * @license <https://github.com/Auvipev/Royal/master/LICENSE>.
+ *
+ * @link    <https://github.com/Auvipev/Royal>.
+ */
