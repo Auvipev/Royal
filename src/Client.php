bc64034774e8c833abcd8f333956d5990bf51a23
Nicholas <nenglish443@outlook.com>
2018-06-05 21:22:02 -0400

    Add client class.

--- src/Client.php
+++ src/Client.php
@@ -1,10 +1,20 @@
 <?php
 
 declare(strict_types=1);
-/*
+/**
  * A PSR-7 compliant http request handler.
  *
  * @license <https://github.com/Auvipev/Royal/master/LICENSE>.
  *
  * @link    <https://github.com/Auvipev/Royal>.
  */
+
+namespace Auvipev\Royal;
+
+/**
+ * @class      Client.
+ * @implements ClientInterface.
+ */
+class Client implements ClientInterface
+{
+}
